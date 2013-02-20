48ed8d3a6c9cde8fa6ebf83fda80273fb7fb2db8
Robert <robert@privatdemail.net>
2013-02-20 13:45:05 +0100

    added license, README and made configuration more verbose

---
+++ README.md
@@ -0,0 +1,11 @@
+WebMPD
+=======
+
+## License
+Most of the things are licensed under the GNU General Public License v2 or (at your option) any later version.
+For details, see COPYING.
+
+### Used libraries
+ * [python-mpd2](https://github.com/Mic92/python-mpd2) v0.4, released under the GNU Lesser General Public License version 3.
+ * [Evoque](http://evoque.gizmojo.org/) v0.5, released under the Academic Free License version 3.0.
+All libraries are delivered with WebMPD.
--- libs/config.py
+++ libs/config.py
@@ -1,6 +1,64 @@
+#  config.py
+#  
+#  Copyright 2013 Robert Knauer <robert@privatdemail.net>
+#  
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#  
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#  
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA 02110-1301, USA.
+#  
+#
+
+################################################################################
+#### This is the main configuration file of WebMPD. ############################
+#### Please change all the configuration variables to fit your environment. ####
+#### If you delete some of the variables, WebMPD won't work anymore. ###########
+################################################################################
+
+
+## Type: string
+## Description: The absolute path to the main directory of WebMPD.
+## IMPORTANT: You have to add a trailing slash and it has to be absolute, not relative.
+## Example: /srv/http/webmpd/
+## WRONG Example: /srv/http/webmpd
+## WRONG Example: ./webmpd/
 path = "/srv/http/webmpd/"
+
+## Type: string
+## Description: The address of the MPD server, without port or protocol.
+## Example: localhost
+## Example: mpd.example.org
+## Example: 127.0.0.1
+## WRONG Example: localhost:6600
 host = "localhost"
+
+## Type: integer
+## Description: The port of the MPD server.
+## MPD's default: 6600
 port = 6600
+
+## Type: string or None
+## Description: The password for the MPD server. If None, WebMPD won't login on the server.
+## MPD's default: None
+## Example: "password123"
 password = None
-timeout = None
+
+## Type: integer or None
+## Description: The timeout for the connection with the MPD server. If None, no timeout.
+timeout = 10
+
+## Type: integer or None
+## Description: The idle timeout for the connection with the MPD server.
+##   Normally we don't need to set this, because we close the connection
+##   a few moments after opening it.
 idletimeout = None
